129db7cd3b3b04834cfbe4392bce350f93316746
Bwly999 <438225686@qq.com>
2022-05-17 10:09:40 +0800

    remove useless dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // import Vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
 import Unocss from 'unocss/vite'
 
 // https://vitejs.dev/config/
@@ -18,22 +16,6 @@ export default defineConfig({
     // Vue({
     //   reactivityTransform: true,
     // }),
-    // https://github.com/antfu/unplugin-auto-import
-    AutoImport({
-      imports: [
-        'vue',
-        'vue/macros',
-        '@vueuse/core',
-      ],
-      dts: true,
-    }),
-
-    // https://github.com/antfu/vite-plugin-components
-    Components({
-      dts: true,
-    }),
-
-    // https://github.com/antfu/unocss
     // see unocss.config.ts for config
     Unocss(),
   ],

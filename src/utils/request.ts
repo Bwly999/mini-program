886860f67d835b97ccce2a2f5ad939846bef2f1d
Bwly999 <438225686@qq.com>
2022-06-28 11:09:14 +0800

    起飞 不用备案了

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,13 +1,21 @@
 import { TOKEN_KEY } from '@/store/user'
-const BASE_URL = 'https://springboot-5jpr-2050213-1311358521.ap-shanghai.run.tcloudbase.com/'
+const BASE_URL = 'https://springboot-5jpr-2050213-1311358521.ap-shanghai.run.tcloudbase.com'
 // const BASE_URL = 'http://www.bwly999.top:8080'
 // const BASE_URL = 'http://139.196.12.152:8080'
 // const BASE_URL = 'http://47.99.200.3:8080'
 
+function qsStringify(data: any) {
+  let qs = ''
+  for (const key in data) {
+    console.log(key)
+    qs += `${key}=${encodeURI(data[key])}&`
+  }
+  return qs.substring(0, qs.length - 1)
+}
 interface Options {
   url: string
   method: 'OPTIONS' | 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'TRACE' | 'CONNECT'
-  data?: string | AnyObject | ArrayBuffer
+  data?: string | AnyObject | ArrayBuffer | any
   hideError?: boolean
 }
 const request = (options: Options) => {
@@ -26,10 +34,20 @@ const request = (options: Options) => {
   }
 
   return new Promise((resolve, reject) => {
+    let url = BASE_URL + options.url
+    if (options.method === 'GET' && options.data !== undefined)
+      url += `?${qsStringify(options.data)}`
+
+    let data
+    if (options.method === 'GET')
+      data = {}
+    else
+      data = options.data
+
     uni.request({
-      url: BASE_URL + options.url,
+      url,
       method: options.method || 'GET',
-      data: options.data || {},
+      data,
       timeout: 8000, // 8秒超时时间，单位 ms
       header: headers,
       success: (res) => {

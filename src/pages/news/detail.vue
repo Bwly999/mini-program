940e26260e78b97986d4902665712da9db1b67db
Bwly999 <438225686@qq.com>
2022-06-29 10:03:28 +0800

    1

--- src/pages/news/detail.vue
+++ src/pages/news/detail.vue
@@ -11,29 +11,17 @@ function loadNewsInfo() {
   const globalVar = useGlobalVarStore()
   if (globalVar.selectedNews.id !== undefined) {
     newsInfo.value = globalVar.selectedNews
+    console.log(newsInfo.value)
   }
   else {
     getNewsById(newsId.value).then((res: any) => {
       newsInfo.value = res.data
+      console.log(newsInfo.value)
     }).catch(() => {
-      newsInfo.value = {
-        id: newsId.value,
-        title: 'title',
-        content: '21123<p>das</p>',
-        time: '1',
-        goodsList: [
-          {
-            id: '62b922b3e4061020f6738d2b',
-            name: '大虾鲜活超大基围虾青岛特大青虾冷冻鲜对虾速冻海虾虾类海鲜水产',
-            coverImgUrl: 'http://122.9.37.63/goods/8e6e5be4-4097-40e0-bbd7-b5b23479aeb7.jpg',
-          },
-          {
-            id: '62ba94dfa816d4170be20814',
-            name: '大虾鲜活超大基围虾青岛特大青虾冷冻鲜对虾速冻海虾虾类海鲜水产',
-            coverImgUrl: 'http://122.9.37.63/goods/f3865b30-8bf3-426f-9585-b6af380bba1f.jpg',
-          },
-        ],
-      }
+      uni.showToast({
+        title: '加载失败',
+        icon: 'error',
+      })
     })
   }
 }
@@ -63,7 +51,7 @@ onLoad((option) => {
       <view class="title">
         推荐商品
       </view>
-      <view class="good flex flex-wrap justify-center">
+      <view class="good pl-2 flex flex-wrap">
         <good-card v-for="v, i in newsInfo?.goodsList" :key="i" class="" :goods="v" css="w-180rpx h-240rpx m2" />
       </view>
     </view>
